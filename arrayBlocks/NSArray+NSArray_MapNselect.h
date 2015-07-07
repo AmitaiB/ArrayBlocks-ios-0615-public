2ab2725c9bd7bc59fbe698ad958d1340122ea288
AmitaiB <blickstein@gmail.com>
2015-07-07 17:27:13 -0400

    adds NSArray+MapNselect impl

---
+++ arrayBlocks/NSArray+NSArray_MapNselect.h
@@ -0,0 +1,17 @@
+//
+//  NSArray+NSArray_MapNselect.h
+//  arrayBlocks
+//
+//  Created by Amitai Blickstein on 7/7/15.
+//  Copyright (c) 2015 Zachary Drossman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (NSArray_MapNselect)
+
+- (void)mapWithOperation:(void (^)(id obj, NSUInteger idx))block;
+
+
+
+@end
---
+++ arrayBlocks/NSArray+NSArray_MapNselect.m
@@ -0,0 +1,18 @@
+//
+//  NSArray+NSArray_MapNselect.m
+//  arrayBlocks
+//
+//  Created by Amitai Blickstein on 7/7/15.
+//  Copyright (c) 2015 Zachary Drossman. All rights reserved.
+//
+
+#import "NSArray+NSArray_MapNselect.h"
+#import "ViewController.h"
+
+@implementation NSArray (NSArray_MapNselect)
+
+- (void)mapWithOperation:(void (^)(id obj, NSUInteger idx))block {
+    
+}
+
+@end
